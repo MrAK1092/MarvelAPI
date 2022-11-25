35bf54243ef7261592cf9cb9b36846e012523eda
MrAK1092 <mrak793@gmail.com>
2022-11-25 19:40:30 +0300

    !!

---
+++ app/src/main/java/ui/PictureFragment.kt
@@ -0,0 +1,26 @@
+package ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.marvelapi.R
+import com.example.marvelapi.databinding.FragmentPictureBinding
+
+
+class PictureFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = FragmentPictureBinding.inflate(inflater)
+        return binding.root
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance() = PictureFragment()
+            }
+    }
